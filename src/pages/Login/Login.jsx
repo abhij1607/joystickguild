0906af97d5ae20e7802c47aeae954a3ce709268a
Abhi Jain <abhijain9807@gmail.com>
2022-05-29 18:33:45 +0530

    [feat] - auth screens

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,65 @@
+import {
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  FormControlLabel,
+  Checkbox,
+} from "@material-ui/core";
+import { Stack } from "@mui/material";
+
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  return (
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid align="center">
+          <Typography variant="h5" component="h1" color="primary">
+            Login to JoystickGuild
+          </Typography>
+          <Typography variant="caption" gutterBottom>
+            Please enter email and password to Login!
+          </Typography>
+        </Grid>
+        <form>
+          <TextField fullWidth label="Email" required />
+          <TextField fullWidth label="Password" required />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Remember Me!"
+            fullWidth
+          />
+          <Stack spacing={1}>
+            <Button
+              type="submit"
+              align="center"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Login
+            </Button>
+            <Button
+              type="submit"
+              align="center"
+              variant="outlined"
+              color="primary"
+              fullWidth
+            >
+              Login with Test Credentials
+            </Button>
+          </Stack>
+        </form>
+        <Typography>
+          Don't have an account?
+          <Link to="/signup">Sign Up</Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
 };
 
 export { Login };
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,42 @@
-const Signup = () => {
-  return <h1>Signup</h1>;
-};
+import { Button, Grid, Paper, TextField, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-export { Signup };
+export const Signup = () => {
+  const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  return (
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid align="center">
+          <Typography variant="h5" component="h1" color="primary">
+            Signup for JoystickGuild
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            Connect with video-game enthusiast around the planet!
+          </Typography>
+        </Grid>
+        <form>
+          <TextField fullWidth label="First Name" />
+          <TextField fullWidth label="Last Name" />
+          <TextField fullWidth label="Email" />
+          <TextField fullWidth label="Password" />
+          <TextField fullWidth label="Confirm Password" />
+
+          <Button
+            style={{ marginTop: "10px" }}
+            type="submit"
+            align="center"
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Sign Up
+          </Button>
+        </form>
+        <Typography>
+          Already have an account
+          <Link to="/login">Sign In</Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
