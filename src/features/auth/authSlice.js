d623ce5b66c9899fe7b8f6b5499470a6385fdbe5
Abhi Jain <abhijain9807@gmail.com>
2022-05-30 12:27:04 +0530

    [feat] - authSlice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { login, signup } from "../../firebase/firebase-auth";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isLoginStatus: "idle",
+  isSignupStatus: "idle",
+};
+
+const authSlice = createSlice({
+  name: "userDetails",
+  initialState,
+  reducers: {
+    logoutUser: (state) => {
+      state.token = null;
+    },
+  },
+  extraReducers: {
+    [signup.pending]: (state) => {
+      state.isSignupStatus = "loading";
+    },
+    [signup.fulfilled]: (state, action) => {
+      state.token = action.payload;
+      localStorage.setItem("token", action.payload);
+      state.isSignupStatus = "fulfilled";
+    },
+    [signup.rejected]: (state) => {
+      state.isSignupStatus = "rejected";
+    },
+
+    [login.pending]: (state) => {
+      state.isLoginStatus = "loading";
+    },
+    [login.fulfilled]: (state, action) => {
+      state.token = action.payload;
+      localStorage.setItem("token", action.payload);
+      state.isLoginStatus = "fulfilled";
+    },
+    [login.rejected]: (state) => {
+      state.isLoginStatus = "rejected";
+    },
+  },
+});
+
+export const { logoutUser } = authSlice.actions;
+
+export default authSlice.reducer;
