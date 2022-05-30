66328789dd602cb5004538f9d32a334c6d123f9a
Abhi Jain <abhijain9807@gmail.com>
2022-05-30 12:29:14 +0530

    [feat] - auth/signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,42 @@
 import { Button, Grid, Paper, TextField, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { handleFormChange } from "utils/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { signup } from "../../firebase/firebase-auth";
 
 export const Signup = () => {
   const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  const initialFormData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [formData, setFormData] = useState(initialFormData);
+  const { firstName, lastName, email, password, confirmPassword } = formData;
+  const isSignupStatus = useSelector(
+    (store) => store.authDetails.isSignupStatus
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await dispatch(signup(formData));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (isSignupStatus === "loading") console.log("loading");
+    if (isSignupStatus === "fulfilled") navigate("/");
+    if (isSignupStatus === "rejected") console.log("rej");
+  }, [isSignupStatus]);
+
   return (
     <Grid>
       <Paper elevation={20} style={paperStyle}>
@@ -14,13 +48,51 @@ export const Signup = () => {
             Connect with video-game enthusiast around the planet!
           </Typography>
         </Grid>
-        <form>
-          <TextField fullWidth label="First Name" />
-          <TextField fullWidth label="Last Name" />
-          <TextField fullWidth label="Email" />
-          <TextField fullWidth label="Password" />
-          <TextField fullWidth label="Confirm Password" />
 
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="First Name"
+            value={firstName}
+            name="firstName"
+            onChange={(e) => handleFormChange(e, setFormData)}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Last Name"
+            value={lastName}
+            name="lastName"
+            onChange={(e) => handleFormChange(e, setFormData)}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Email"
+            type="email"
+            value={email}
+            name="email"
+            onChange={(e) => handleFormChange(e, setFormData)}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            value={password}
+            name="password"
+            onChange={(e) => handleFormChange(e, setFormData)}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Confirm Password"
+            type="password"
+            value={confirmPassword}
+            name="confirmPassword"
+            onChange={(e) => handleFormChange(e, setFormData)}
+            required
+          />
           <Button
             style={{ marginTop: "10px" }}
             type="submit"
@@ -32,6 +104,7 @@ export const Signup = () => {
             Sign Up
           </Button>
         </form>
+
         <Typography>
           Already have an account
           <Link to="/login">Sign In</Link>
