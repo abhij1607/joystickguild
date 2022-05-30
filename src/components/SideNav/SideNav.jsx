6cddfcd7813f2238270c3dea01ef06d8404e700f
Abhi Jain <abhijain9807@gmail.com>
2022-05-31 00:14:52 +0530

    [feat] - SideNav

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,60 @@
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import RocketOutlinedIcon from "@mui/icons-material/RocketOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Stack, Button } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
+import "./SideNav.css";
+
+import { NavLink } from "react-router-dom";
+
+const useStyles = makeStyles({
+  link: {
+    "&.active": {
+      background: "lightblue",
+    },
+  },
+});
+
+const SidebarLink = ({ text, Icon, url }) => {
+  const classes = useStyles();
+  return (
+    <Button to={url} component={NavLink} className={`${classes.link} navlink`}>
+      <Icon /> {text}
+    </Button>
+  );
+};
+
+export const SideNav = () => {
+  return (
+    <Stack spacing={3}>
+      <SidebarLink Icon={HomeOutlinedIcon} text="Home" url="/" />
+      <SidebarLink Icon={RocketOutlinedIcon} text="Explore" url="/explore" />
+      <SidebarLink
+        Icon={NotificationsNoneOutlinedIcon}
+        text="Notifications"
+        url="/notification"
+      />
+      <SidebarLink
+        Icon={BookmarkBorderOutlinedIcon}
+        text="Bookmarks"
+        url="bookmarks"
+      />
+      <SidebarLink
+        Icon={AccountCircleOutlinedIcon}
+        text="Profile"
+        url="profile"
+      />
+      <Button
+        type="button"
+        align="center"
+        variant="outlined"
+        color="primary"
+        fullWidth
+      >
+        Logout
+      </Button>
+    </Stack>
+  );
+};
