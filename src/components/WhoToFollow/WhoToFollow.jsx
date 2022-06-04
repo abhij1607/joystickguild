efb314f298184f476382339c4b024458abea5324
Abhi Jain <abhijain9807@gmail.com>
2022-06-05 00:06:30 +0530

    [refactor] - who to follow panel

--- src/components/WhoToFollow/WhoToFollow.jsx
+++ src/components/WhoToFollow/WhoToFollow.jsx
@@ -1,56 +1,7 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListSubheader from "@mui/material/ListSubheader";
-
-import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-
-import { followUser, unfollowUser } from "../../firebase/firestore-requests";
-import {
-  updateUserFollowing,
-  updateUserUnfollow,
-} from "features/user/userSlice";
-
-const SuggestedUserCard = ({ user, token }) => {
-  const { firstName, profilePicture } = user.data;
-
-  const dispatch = useDispatch();
-
-  const handleFollow = async () => {
-    try {
-      await followUser(user.id, token);
-      dispatch(updateUserFollowing(user.id));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleUnfollow = async () => {
-    try {
-      await unfollowUser(user.id, token);
-      dispatch(updateUserUnfollow(user.id));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <ListItem
-      secondaryAction={
-        <Button edge="end" aria-label="follow" onClick={handleFollow}>
-          Follow
-        </Button>
-      }
-    >
-      <ListItemAvatar>
-        <Avatar src={profilePicture} />
-      </ListItemAvatar>
-      <ListItemText primary={firstName} />
-    </ListItem>
-  );
-};
+import { SuggestedUserCard } from "components/SuggestedUserCard/SuggestedUserCard";
 
 export const WhoToFollow = () => {
   const { users } = useSelector((store) => store.allUsers);
@@ -58,20 +9,17 @@ export const WhoToFollow = () => {
   const {
     userDetails: { following },
   } = useSelector((store) => store.userDetails);
-  console.log(users, "users");
 
   const suggestions = users.filter(
     (user) => user.id !== token && !following?.following?.includes(user.id)
   );
 
   return (
-    <>
-      <List>
-        <ListSubheader component="div">Who To follow</ListSubheader>
-        {suggestions.map((user) => {
-          return <SuggestedUserCard key={user.id} user={user} token={token} />;
-        })}
-      </List>
-    </>
+    <List>
+      <ListSubheader component="div">Who To follow</ListSubheader>
+      {suggestions.map((user) => {
+        return <SuggestedUserCard key={user.id} user={user} token={token} />;
+      })}
+    </List>
   );
 };
