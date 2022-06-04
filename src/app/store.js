dab16c9e9f48793ab56989cc0d15bbf43a8ca612
Abhi Jain <abhijain9807@gmail.com>
2022-06-04 16:29:07 +0530

    [feat] - allUsers Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "features/auth/authSlice";
 import userReducer from "features/user/userSlice";
+import allUsersReducer from "features/allUsers/allUserSlice";
 
 export const store = configureStore({
   reducer: {
     authDetails: authReducer,
     userDetails: userReducer,
+    allUsers: allUsersReducer,
   },
 });
