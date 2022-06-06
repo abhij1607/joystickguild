4202a678a46e3989983af3c129b17e5580ef076f
Abhi Jain <abhijain9807@gmail.com>
2022-06-06 10:29:45 +0530

    [feat] - fetchAllPosts & update in store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "features/auth/authSlice";
 import userReducer from "features/user/userSlice";
 import allUsersReducer from "features/allUsers/allUserSlice";
+import postReducer from "features/post/postSlice";
 
 export const store = configureStore({
   reducer: {
     authDetails: authReducer,
     userDetails: userReducer,
     allUsers: allUsersReducer,
+    post: postReducer,
   },
 });
