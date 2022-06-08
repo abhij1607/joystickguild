4da72190fa7d400ece227a2135741488a942bafe
Abhi Jain <abhijain9807@gmail.com>
2022-06-08 12:35:12 +0530

    [feat] - posts in bookmarks

--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,3 +1,32 @@
+import { Post } from "components/Post/Post";
+import { useSelector } from "react-redux";
+import { sortPosts } from "utils/sortBy";
+import { useState } from "react";
+import Divider from "@mui/material/Divider";
+import { FeedSortBy } from "components/SortBy/SortBy";
+
 export const Bookmarks = () => {
-  return <h1>Bookmarks</h1>;
+  const [sortBy, setSortBy] = useState("LATEST");
+
+  const { token } = useSelector((store) => store.authDetails);
+  const {
+    userDetails: { bookmarks },
+  } = useSelector((store) => store.userDetails);
+  const { posts } = useSelector((store) => store.post);
+
+  const filteredPostsByBookmarks = posts?.filter((post) =>
+    bookmarks?.bookmarks?.includes(post?.id)
+  );
+
+  const sortedPosts = sortPosts(filteredPostsByBookmarks, sortBy);
+
+  return (
+    <>
+      <FeedSortBy setSortBy={setSortBy} sortBy={sortBy} />
+      <Divider />
+      {sortedPosts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
 };
